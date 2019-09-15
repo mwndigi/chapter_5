3ebbb8f7ef71a5beac210228128e5f0205b4d3d8
Mikkel Wessel Nielsen <mwn.digi@cbs.dk>
2019-09-15 22:01:05 +0200

    exercises

---
+++ yourOwnLoop.js
@@ -0,0 +1,13 @@
+// Your own loop
+// Write a higher-order function loop that provides something like a for loop
+// statement. It takes a value, a test function, an update function, and a body
+// function. Each iteration, it first runs the test function on the current loop value
+// and stops if that returns false. Then it calls the body function, giving it the
+// current value. Finally, it calls the update function to create a new value and
+// starts from the beginning.
+// When defining the function, you can use a regular loop to do the actual
+// looping.
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
